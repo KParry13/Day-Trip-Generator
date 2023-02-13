bb4d22a59d28959d174f2c3c936be8741a6ba035
Kayla <k_meijer_10@yahoo.com>
2023-02-13 17:52:59 -0500

    Run day trip First Draft

--- DayTripGen.py
+++ DayTripGen.py
@@ -3,7 +3,7 @@ import random
 
 Destination = ['Chicago', 'Louisville', 'Gatlinburg', 'Myrtle Beach']
 Restaurant = ['Cafe Lulu', 'Derby Bowl', 'Wild Bears', 'Sea Captains House']
-Mode_of_Transportation = ['Train', 'Helicopter', 'Skyline', 'Motorcycle']
+Transportation = ['Train', 'Helicopter', 'Skyline', 'Motorcycle']
 Entertainment = ['Musical', 'Hiking', 'Zipline', 'Aquarium']
 
 
@@ -11,58 +11,75 @@ def Trip_options(list_trip):
     select_trip= random.choice(list_trip)
     return select_trip
 
-my_destiantion = Trip_options(Destination)
-print ("Destination: ",my_destiantion)
+def run_day_trip():
+    my_destiantion = Trip_options(Destination)
+    print ("Destination: ",my_destiantion)
 
-my_restaurant = Trip_options(Restaurant)
-print ("Restaurant: ",my_restaurant)
+    my_restaurant = Trip_options(Restaurant)
+    print ("Restaurant: ",my_restaurant)
 
-my_transportaion = Trip_options(Mode_of_Transportation)
-print ("Transportaion: ",my_transportaion)
+    my_transportaion = Trip_options(Transportation)
+    print ("Transportaion: ",my_transportaion)
 
-my_entertainment = Trip_options(Entertainment)
-print ("Entertainment: ",my_entertainment)
+    my_entertainment = Trip_options(Entertainment)
+    print ("Entertainment: ",my_entertainment)
 
-Y = 'Great Selection!'
 
-print('Does this sound like a trip for you?')
-answer = input('')
-while answer == 'N':
-    print('What is not to your liking?')
+
+    print('Does this sound like a trip for you?')
     answer = input('')
-    if answer == 'Destination':
-        my_destiantion = Trip_options(Destination)
-        print ("Destination: ",my_destiantion)
-    elif answer == 'Restaurant':
-        my_restaurant = Trip_options(Restaurant)
-        print ("Restaurant: ",my_restaurant)
-    elif answer == 'Mode of Transportation':
-        my_transportaion = Trip_options(Mode_of_Transportation)
-        print ("Transportaion: ",my_transportaion)
-    elif answer == 'Entertainment':
-        my_entertainment = Trip_options(Entertainment)
-        print ("Entertainment: ",my_entertainment)  
-while answer == 'Y':
-    print('Nice!')
-    break
-
-
-def confirmation(options):
-    select_trip = options
-    options=confirmation
-    for options in Trip_options:
-    
-        print ("Destination: ",my_destiantion)
-        print ("Restaurant: ",my_restaurant)
-        print ("Transportaion: ",my_transportaion)
-        print ("Entertainment: ",my_entertainment)
-
-    print ('Can we make your reservations?')
-    answer = input('Y or N')
     while answer == 'N':
-        print('Start selections over.')
-    while answer == 'Y':
-        print('Pack your bags! Your plans are reserved!')
+        print('What is not to your liking?')
+        answer = input('')
+        if answer == 'Destination':
+            my_destiantion = Trip_options(Destination)
+            print ("Destination: ",my_destiantion)
+        elif answer == 'Restaurant':
+            my_restaurant = Trip_options(Restaurant)
+            print ("Restaurant: ",my_restaurant)
+        elif answer == 'Transportation':
+            my_transportaion = Trip_options(Transportation)
+            print ("Transportaion: ",my_transportaion)
+        elif answer == 'Entertainment':
+            my_entertainment = Trip_options(Entertainment)
+            print ("Entertainment: ",my_entertainment)  
+
+        print('How does this sound?')
+    answer = input('')
+    print('What would you like to change?')
+        
+    if answer == 'Y':
+        print('Great Choice!')
+        
+    answer = 'N'
+    while answer == 'N':
+
+        print(f"Destination: {my_destiantion}")
+        print("Restaurant: ",my_restaurant)
+        print("Transportaion: ",my_transportaion)
+        print("Entertainment: ",my_entertainment)
+        print ('confirm your trip')
+        print ('Can we make your reservations?')
+        answer = input('Y or N')
+        if answer == 'N':
+            print('Start selections over.')
+                    
+            my_destiantion = Trip_options(Destination)
+          
+
+            my_restaurant = Trip_options(Restaurant)
+           
+
+            my_transportaion = Trip_options(Transportation)
+           
+
+            my_entertainment = Trip_options(Entertainment)
+
+
+            
+
+        elif answer == 'Y':
+            print('Pack your bags! Your plans are reserved!')
 
 
 
@@ -70,3 +87,4 @@ def confirmation(options):
     
 
     
+run_day_trip()

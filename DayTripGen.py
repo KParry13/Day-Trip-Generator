33807c5546d24a878c863245744525dcdf0f23b9
Kayla <k_meijer_10@yahoo.com>
2023-02-13 14:51:52 -0500

    Created Reselect Option

--- DayTripGen.py
+++ DayTripGen.py
@@ -7,7 +7,6 @@ Mode_of_Transportation = ['Train', 'Helicopter', 'Skyline', 'Motorcycle']
 Entertainment = ['Musical', 'Hiking', 'Zipline', 'Aquarium']
 
 
-
 def Trip_options(list_trip):
     select_trip= random.choice(list_trip)
     return select_trip
@@ -24,12 +23,50 @@ print ("Transportaion: ",my_transportaion)
 my_entertainment = Trip_options(Entertainment)
 print ("Entertainment: ",my_entertainment)
 
+Y = 'Great Selection!'
 
-def determine_satisfaction(current_trip, Trip_options):
-    user_satisfied= input('Are you satisfied with this trip?')
-    yes = ('Awesome! Pack your bags!')
-    no = input('What would you like to change?')
-    if yes:
-        print(yes)
+print('Does this sound like a trip for you?')
+answer = input('')
+while answer == 'N':
+    print('What is not to your liking?')
+    answer = input('')
+    if answer == 'Destination':
+        my_destiantion = Trip_options(Destination)
+        print ("Destination: ",my_destiantion)
+    elif answer == 'Restaurant':
+        my_restaurant = Trip_options(Restaurant)
+        print ("Restaurant: ",my_restaurant)
+    elif answer == 'Mode of Transportation':
+        my_transportaion = Trip_options(Mode_of_Transportation)
+        print ("Transportaion: ",my_transportaion)
+    elif answer == 'Entertainment':
+        my_entertainment = Trip_options(Entertainment)
+        print ("Entertainment: ",my_entertainment)  
+while answer == 'Y':
+    print('Nice!')
+    break
+
+
+def confirmation(options):
+    select_trip = options
+    options=confirmation
+    for options in Trip_options:
+    
+        print ("Destination: ",my_destiantion)
+        print ("Restaurant: ",my_restaurant)
+        print ("Transportaion: ",my_transportaion)
+        print ("Entertainment: ",my_entertainment)
+
+    print ('Can we make your reservations?')
+    answer = input('Y or N')
+    while answer == 'N':
+        print('Start selections over.')
+    while answer == 'Y':
+        print('Pack your bags! Your plans are reserved!')
+
+
+
+
+    
 
     
